99bee7d737a2c6dbc6023ae82ad4b86175042f54
pavel-sobchenko <pavel.sobchanka@gmail.com>
2023-12-26 00:46:26 +0100

    added user creation to db script

--- db-script.sql
+++ db-script.sql
@@ -1,33 +1,49 @@
+-- Enable uuid-ossp extension
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- Create 'carts' table with automatically generated UUID
+-- Create 'users' table with automatically generated UUID
+CREATE TABLE users (
+  id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE
+);
+
+-- Create 'carts' table with foreign key relationship to 'users'
 CREATE TABLE carts (
   id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  user_id UUID NOT NULL,
+  user_id UUID REFERENCES users(id),
   created_at DATE NOT NULL,
   updated_at DATE NOT NULL,
   status VARCHAR(20) CHECK (status IN ('OPEN', 'ORDERED'))
 );
 
--- Insert data into 'carts' table
-INSERT INTO carts (user_id, created_at, updated_at, status)
-VALUES
-  (uuid_generate_v4(), '2023-01-01', '2023-01-01', 'OPEN'),
-  (uuid_generate_v4(), '2023-01-02', '2023-01-02', 'ORDERED');
-
--- Create 'cart_items' table with automatically generated UUID
-CREATE TABLE cart_items (
+-- Create 'orders' table with foreign key relationships to 'users' and 'carts'
+CREATE TABLE orders (
+  id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  user_id UUID REFERENCES users(id),
   cart_id UUID REFERENCES carts(id),
-  product_id UUID,
-  count INTEGER,
-  PRIMARY KEY (cart_id, product_id)
+  payment JSONB,
+  delivery JSONB,
+  comments TEXT,
+  status VARCHAR(20) CHECK (status IN ('ORDERED', 'OPEN')),
+  total NUMERIC(10, 2) NOT NULL
 );
 
--- Insert data into 'cart_items' table
-INSERT INTO cart_items (cart_id, product_id, count)
-VALUES
-  ((SELECT id FROM carts ORDER BY created_at LIMIT 1), uuid_generate_v4(), 3),
-  ((SELECT id FROM carts ORDER BY created_at LIMIT 1), uuid_generate_v4(), 2),
-  ((SELECT id FROM carts ORDER BY created_at DESC LIMIT 1), uuid_generate_v4(), 1),
-  ((SELECT id FROM carts ORDER BY created_at DESC LIMIT 1), uuid_generate_v4(), 4);
+-- Insert sample user data
+INSERT INTO users (username, email) VALUES
+  ('user1', 'user1@example.com'),
+  ('user2', 'user2@example.com');
 
+-- Insert sample cart data
+INSERT INTO carts (user_id, created_at, updated_at, status) VALUES
+  ((SELECT id FROM users WHERE username = 'user1'), '2023-01-01', '2023-01-01', 'OPEN'),
+  ((SELECT id FROM users WHERE username = 'user2'), '2023-01-02', '2023-01-02', 'OPEN');
+
+-- Insert sample order data
+INSERT INTO orders (user_id, cart_id, payment, delivery, comments, status, total) VALUES
+  ((SELECT id FROM users WHERE username = 'user1'), 
+  (SELECT id FROM carts WHERE user_id = (SELECT id FROM users WHERE username = 'user1')), 
+  '{"currency": "USD"}', '{"address": "123 Main St"}', 'Order comments', 'ORDERED', 100.50),
+  ((SELECT id FROM users WHERE username = 'user2'), 
+  (SELECT id FROM carts WHERE user_id = (SELECT id FROM users WHERE username = 'user2')), 
+  '{"currency": "USD"}', '{"address": "456 Oak St"}', 'Another order comments', 'ORDERED', 75.25);
